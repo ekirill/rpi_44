6acf8ca05331a948d1ade109ae5f8f30c40e9ddc
Kirill Emelyanov <ekirill@ekirill.ru>
2019-11-06 02:31:16 +0300

    refactoring

--- lighter/lighter.py
+++ lighter/lighter.py
@@ -64,22 +64,25 @@ SWITCHER_PIN = 26
 TZ = pytz.timezone('Europe/Moscow')
 
 LIGHT_ON_MIN_TIME = datetime.time(19, 0, tzinfo=TZ)
-LIGHT_ON_MAX_DELAY = 60
+LIGHT_ON_MAX_DELAY = 60 * 60
 
-LIGHT_OFF_MIN_TIME = datetime.time(0, 0, tzinfo=TZ)
-LIGHT_OFF_MAX_DELAY = 60
+LIGHT_OFF_MIN_TIME = datetime.time(0, 30, tzinfo=TZ)
+LIGHT_OFF_MAX_DELAY = 2 * 60 * 60
 
 
 class Lamp:
-    ON = 1
-    OFF = 0
+    ON = 'ON'
+    OFF = 'OFF'
 
     MIN_SWITCH_DELAY = 10
 
     state = OFF
     last_switch_time = 0
 
-    def _set_light_state(self, state):
+    def set_state(self, state):
+        if self.state == state:
+            return
+
         if time.time() - self.last_switch_time < self.MIN_SWITCH_DELAY:
             logger.warning('Switched less then 10 seconds ago, ignoring switch')
             return
@@ -92,15 +95,13 @@ class Lamp:
         self.state = state
         self.last_switch_time = time.time()
 
-    def switch_off(self):
-        if self.state == self.ON:
-            logger.debug('Lights OFF')
-            self._set_light_state(self.OFF)
+    @property
+    def is_on(self):
+        return self.state == self.ON
 
-    def switch_on(self):
-        if self.state == self.OFF:
-            logger.debug('Lights ON')
-            self._set_light_state(self.ON)
+    @property
+    def is_off(self):
+        return self.state == self.OFF
 
 
 class Sensor:
@@ -108,6 +109,7 @@ class Sensor:
         self.photo_sensor_channel = photo_sensor_channel
         self.temp_sensor_channel = temp_sensor_channel
         self.i2c = i2c
+        self.latest_values = []
 
     def _read_sensor_value(self, chan):
         value = self.i2c.read_chan(chan)
@@ -126,52 +128,40 @@ class Sensor:
                 daytime = dt
                 break
 
-        self.get_temp()
+        # need 3 equal values to assume that night has come
+        self.latest_values.append(daytime)
+        if len(set(self.latest_values)) > 1:
+            daytime = self.latest_values[-2]
+
+        if len(self.latest_values) > 1:
+            self.latest_values = self.latest_values[1:3]
+
         logger.debug("Photo sensor value is: %d, assuming it is daytime: %s", value, daytime)
 
         return daytime
 
-    def get_temp(self):
-        value = self._read_sensor_value(self.temp_sensor_channel)
-
-        logger.debug("Temp sensor value is: %d", value)
-
-        return value
-
-
-class LighterState:
-    WAIT_NIGHT = 0
-    WAIT_ON = 1
-    WAIT_OFF = 2
-    WAIT_MORNING = 3
-
 
 class Lighter:
-    state_changed = time.time()
-    state = LighterState.WAIT_NIGHT
+    state_change_plan = None
 
     def __init__(self):
         i2c = I2C(I2C_DEV_ADDR, I2C_BUS_ID)
         self.light_sensor = Sensor(i2c, PHOTO_SENSOR_CHAN, TEMP_SENSOR_CHAN)
         self.lamp = Lamp()
-        self.set_state(LighterState.WAIT_MORNING)
 
-    @property
-    def seconds_from_state_changed(self):
-        return time.time() - self.state_changed
+        now = Lighter.get_now()
+        desired_state = self.get_desired_state(now)
 
-    def set_state(self, state):
-        self.state = state
-        self.state_changed = time.time()
-
-        if state == LighterState.WAIT_OFF:
-            self.lamp.switch_on()
-        else:
-            self.lamp.switch_off()
+        logger.info('Initializing Lighter. Desired state for now is `%s`', desired_state)
+        self.lamp.set_state(desired_state)
 
     @staticmethod
-    def get_today_turn_on_min_time():
-        turn_on_min_time = datetime.datetime.now(tz=TZ).replace(
+    def get_now():
+        return datetime.datetime.now(tz=TZ)
+
+    @staticmethod
+    def get_today_turn_on_min_time(now):
+        turn_on_min_time = now.replace(
             hour=LIGHT_ON_MIN_TIME.hour,
             minute=LIGHT_ON_MIN_TIME.minute,
             second=0,
@@ -180,79 +170,133 @@ class Lighter:
         return turn_on_min_time
 
     @staticmethod
-    def get_today_turn_off_min_time():
-        turn_off_min_time = datetime.datetime.now(tz=TZ).replace(
+    def get_next_turn_on_min_time(now):
+        turn_on_min_time = Lighter.get_today_turn_on_min_time(now)
+        if now > turn_on_min_time:
+            turn_on_min_time += datetime.timedelta(days=1)
+
+        return turn_on_min_time
+
+    @staticmethod
+    def get_tommorow_turn_on_min_time(now):
+        turn_on_min_time = Lighter.get_today_turn_on_min_time(now)
+        turn_on_min_time += datetime.timedelta(days=1)
+
+        return turn_on_min_time
+
+    @staticmethod
+    def get_today_turn_off_min_time(now):
+        turn_off_min_time = now.replace(
             hour=LIGHT_OFF_MIN_TIME.hour,
             minute=LIGHT_OFF_MIN_TIME.minute,
             second=0,
             microsecond=0,
         )
+
         return turn_off_min_time
 
     @staticmethod
-    def get_next_turn_off_min_time():
-        turn_off_min_time = Lighter.get_today_turn_off_min_time()
-        if turn_off_min_time < datetime.datetime.now(tz=TZ):
+    def get_next_turn_off_min_time(now):
+        turn_off_min_time = Lighter.get_today_turn_off_min_time(now)
+        if now > turn_off_min_time:
             turn_off_min_time += datetime.timedelta(days=1)
 
         return turn_off_min_time
 
+    @staticmethod
+    def get_desired_state_by_time(now):
+        sorted_times = sorted([
+            (now.time(), 'now'),
+            (LIGHT_ON_MIN_TIME, Lamp.ON),
+            (LIGHT_OFF_MIN_TIME, Lamp.OFF),
+        ])
+
+        # we need to know what state is desired before now
+        desired_state = None
+        for _, state in sorted_times:
+            if state == 'now':
+                break
+            desired_state = state
+
+        if not desired_state:
+            desired_state = sorted_times[-1][1]
+
+        return desired_state
+
+    def get_desired_state_by_sensor(self):
+        daytime = self.light_sensor.get_daytime()
+        if daytime == Daytime.NIGHT:
+            return Lamp.ON
+        else:
+            return Lamp.OFF
+
+    def get_desired_state(self, now):
+        desired_state_by_time = Lighter.get_desired_state_by_time(now)
+        desired_state_by_sensor = self.get_desired_state_by_sensor()
+
+        if desired_state_by_sensor == desired_state_by_time:
+            final_desired = desired_state_by_sensor
+        else:
+            final_desired = desired_state_by_time
+
+        logger.debug(
+            'Desired by time: `%s`, desired by sensor: `%s`, final desired = `%s`',
+            desired_state_by_time, desired_state_by_sensor, final_desired
+        )
+        return final_desired
+
+    def execute_planned_changes(self, now):
+        if not self.state_change_plan:
+            return False
+
+        desired_time, desired_state = self.state_change_plan
+        if now > desired_time:
+            logger.info('Change planned for now, switching lamp to `%s`', desired_state)
+            self.lamp.set_state(desired_state)
+        else:
+            logger.debug('Change planned at %s, waiting', desired_time)
+            return True
+
+        self.state_change_plan = None
+        return True
+
+    def plan_state_change(self, now, desired_state):
+        if desired_state == Lamp.ON:
+            time_to_off = Lighter.get_next_turn_off_min_time(now)
+            latest_time_to_on = now + datetime.timedelta(seconds=LIGHT_ON_MAX_DELAY)
+            latest_time_change_state = min(time_to_off, latest_time_to_on)
+        else:
+            time_to_on = Lighter.get_next_turn_on_min_time(now)
+            latest_time_to_off = now + datetime.timedelta(seconds=LIGHT_OFF_MAX_DELAY)
+            latest_time_change_state = min(time_to_on, latest_time_to_off)
+
+        if latest_time_change_state < now:
+            logger.error(
+                'Latest time for switching `%s` is %s, but its %s already',
+                desired_state, latest_time_change_state, now
+            )
+            return
+
+        desired_delay = int((latest_time_change_state - now).total_seconds() * random.random())
+        desired_time = now + datetime.timedelta(seconds=desired_delay)
+
+        self.state_change_plan = (desired_time, desired_state)
+        logger.info('Planned switching lamp to `%s` at %s', desired_state, desired_time)
+
     def run(self):
         while True:
-            now = datetime.datetime.now(tz=TZ)
+            time.sleep(60)
+            now = Lighter.get_now()
 
-            if self.state == LighterState.WAIT_NIGHT:
-                daytime = self.light_sensor.get_daytime()
-                if daytime == Daytime.NIGHT:
-                    if now > Lighter.get_today_turn_on_min_time():
-                        if now >= Lighter.get_today_turn_off_min_time():
-                            self.set_state(LighterState.WAIT_MORNING)
-                            logger.warning("Night detected, but it's time to sleep. Waiting for morning")
-                            time.sleep(60)
-                            continue
+            if self.execute_planned_changes(now):
+                continue
 
-                        self.set_state(LighterState.WAIT_ON)
-
-                        desired_turn_on_time = now + datetime.timedelta(seconds=random.random() * LIGHT_ON_MAX_DELAY)
-                        time_to_sleep = max((desired_turn_on_time - now).total_seconds(), 10)
-
-                        turn_on_time = now + datetime.timedelta(seconds=int(time_to_sleep))
-
-                        logger.info('Night detected. Light will be turned on at %s', turn_on_time)
-
-                        time.sleep(time_to_sleep)
-                    else:
-                        logger.debug('Night detected, but its too early. Doing nothing')
-
-                time.sleep(60)
-            elif self.state == LighterState.WAIT_ON:
-                self.lamp.switch_on()
-                self.set_state(LighterState.WAIT_OFF)
-
-                turn_off_min_time = Lighter.get_next_turn_off_min_time()
-                desired_turn_off_time = (
-                    turn_off_min_time + datetime.timedelta(seconds=random.random() * LIGHT_OFF_MAX_DELAY)
-                )
-                time_to_sleep = max((desired_turn_off_time - now).total_seconds(), 10)
-                turn_off_time = now + datetime.timedelta(seconds=int(time_to_sleep))
-                logger.info('Light turned on. Will be turned off at %s', turn_off_time)
-
-                time.sleep(time_to_sleep)
-            elif self.state == LighterState.WAIT_OFF:
-                self.lamp.switch_off()
-                self.set_state(LighterState.WAIT_MORNING)
-                logger.info('Light turned off. Waiting for morning')
-                time.sleep(60)
-            elif self.state == LighterState.WAIT_MORNING:
-                daytime = self.light_sensor.get_daytime()
-                if daytime != Daytime.NIGHT:
-                    self.set_state(LighterState.WAIT_NIGHT)
-                    logger.info('Morning. Waiting for night.')
-
-                time.sleep(60)
+            desired_state = self.get_desired_state(now)
+            if self.lamp.state != desired_state:
+                self.plan_state_change(now, desired_state)
 
     def cleanup(self):
-        self.lamp.switch_off()
+        self.lamp.set_state(Lamp.OFF)
         logger.info('STOPPED AUTO LIGHTER')
 
 
